73aff1738844431103f8c15d394b343021814b73
Vostenznuk <dimacontacto94@gmail.com>
2023-12-25 11:57:17 +0100

    ajustes en el primer ejemplo

--- CUADRADOS/script.js
+++ CUADRADOS/script.js
@@ -3,21 +3,21 @@ const canvas = canvas_propiedades.getContext('2d')
 
 canvas_propiedades.width = window.innerWidth / 2;
 canvas_propiedades.height = window.innerHeight / 2;
+//Propiedad de lo que se vaya a pintar
+canvas.fillStyle = 'red'
+//Propiedades del tablero
+canvas.strokeStyle = 'green'
+canvas.lineWidth = 2
+const COLUMNAS = 10;
+const FILAS = 8;
+const PX = 50;
 
-canvas.fillStyle = 'green'
-canvas.strokeStyle = 'black'
-canvas.lineWidth = 1
-
-
-for (let i = 0; i < 8; i++) {
-    for (let j = 0; j < 8; j++) {
-        canvas.strokeRect(i*50,j*50,50,50);
+for (let i = 0; i < COLUMNAS; i++) {
+    for (let j = 0; j < FILAS; j++) {
+        canvas.strokeRect(i*PX, j*PX, PX, PX);
     }
 }
-
-//canvas.fillRect(250,250,50,50)
-
-
+canvas.fillRect(250,250, PX, PX)
 console.log(canvas);
 
 
